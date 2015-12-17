4440af06fb61c9fa1eadad457a9454cbcf853096
Arno Velds <avelds@gmail.com>
2015-12-17 16:29:21 +0100

    removed shebang

--- 16/16b.pl
+++ 16/16b.pl
@@ -1,4 +1,3 @@
-#!/usr/bin/env perl 
 sub a{my ($v, $m) = @_;	sub{$m ? ($m<0 ? $_[0]<$v :$_[0]>$v) :$_[0] == $v}}
 my $b=sub{1};
 my %m = (chi=>a(3),cat=>a(7,1),sam=>a(2),pom=>a(3,-1),aki=>a(0),viz=>a(0),gol=>a(5,-1),tre=>a(3,1),car=>a(2),per=>a(1));
