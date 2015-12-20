869f8f658abb6ac2b26398a7dbe279efa50fef6b
Arno Velds <avelds@gmail.com>
2015-12-20 12:23:16 +0100

    remove useless loop

--- 20/src/bin/20b.rs
+++ 20/src/bin/20b.rs
@@ -5,16 +5,14 @@ fn main() {
     let n = 33100000;
     //worst case elve 3_310_000 delivers 33_100_000 presents at house 3_310_000
     let mut v = vec![10; 3_310_000];
-    loop {
-        for e in 2 .. 3_310_001 {
-            let maxpos = if e*50 < 3_310_000 { e*50} else {3_310_000 -1};
-            for p in (e .. maxpos).step_by(e) {
-                v[p-1] += e*11;
-            }
-            if v[e-1] > n {
-                println!("House {} has more than {} presents({})", e, n, v[e-1]);
-                exit(0);
-            }
+    for e in 2 .. 3_310_001 {
+        let maxpos = if e*50 < 3_310_000 { e*50} else {3_310_000 -1};
+        for p in (e .. maxpos).step_by(e) {
+            v[p-1] += e*11;
+        }
+        if v[e-1] > n {
+            println!("House {} has more than {} presents({})", e, n, v[e-1]);
+            exit(0);
         }
     }
 }
