7932efe4d3bb84f8c9dea0d7db32abe1d98a717e
Arno Velds <a.velds@nki.nl>
2018-12-14 11:17:52 +0100

    day 14, no tricks

---
+++ 2018/day_14/src/main.rs
@@ -0,0 +1,69 @@
+fn recipe_scores(n: usize) -> String {
+    let mut v = vec![3,7];
+    let mut elf_1 = 0;
+    let mut elf_2 = 1;
+
+    while v.len() < n + 10 {
+        let recipe_1 = v[elf_1];
+        let recipe_2 = v[elf_2];
+        let sum = recipe_1 + recipe_2;
+        if sum >= 10 {
+            v.push(1);
+            v.push(sum - 10);
+        } else {
+            v.push(sum);
+        }
+        elf_1 = (elf_1 + 1 + recipe_1) % v.len();
+        elf_2 = (elf_2 + 1 + recipe_2) % v.len();
+    }
+    v[n..].iter().take(10).map(|i| format!("{}", i)).collect()
+}
+
+fn recipes_before(n: String) -> usize {
+    let pattern: Vec<usize> = n.chars().map(|c| c.to_digit(10).unwrap() as usize).collect();
+    let l = pattern.len();
+
+    let mut v = vec![3usize,7];
+    let mut elf_1 = 0;
+    let mut elf_2 = 1;
+
+    while v.len() < l || &v[v.len()-l..] != &pattern[..] {
+        let recipe_1 = v[elf_1];
+        let recipe_2 = v[elf_2];
+        let sum = recipe_1 + recipe_2;
+        if sum >= 10 {
+            v.push(1);
+            if v.len() >= l && &v[v.len()-l..] == &pattern[..] {
+                break
+            }
+            v.push(sum - 10);
+        } else {
+            v.push(sum);
+        }
+        elf_1 = (elf_1 + 1 + recipe_1) % v.len();
+        elf_2 = (elf_2 + 1 + recipe_2) % v.len();
+    }
+    v.len() - l 
+}
+
+fn main() {
+    let input = 306281;
+    println!("14a: Recipies after {} recipies is {}", input, recipe_scores(input));
+    println!("14b: Recipies before input sequnce is {}", recipes_before(format!("{}", input)));
+}
+
+#[test]
+fn test() {
+    assert_eq!(recipe_scores(9), "5158916779");
+    assert_eq!(recipe_scores(5), "0124515891");
+    assert_eq!(recipe_scores(18), "9251071085");
+    assert_eq!(recipe_scores(2018), "5941429882");
+}
+
+#[test]
+fn test2() {
+    assert_eq!(recipes_before("51589".to_owned()), 9);
+    assert_eq!(recipes_before("01245".to_owned()), 5);
+    assert_eq!(recipes_before("92510".to_owned()), 18);
+    assert_eq!(recipes_before("59414".to_owned()), 2018);
+}
