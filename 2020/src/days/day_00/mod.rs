0ecb61ee52fe29a0de09f046bf274e1f19b1a81f
Arno Velds <avelds@gmail.com>
2020-11-30 19:57:29 +0100

    small trait change

--- 2020/src/days/day_00/mod.rs
+++ 2020/src/days/day_00/mod.rs
@@ -9,8 +9,8 @@ pub struct Solution {
 impl Problem for Solution {
     //fn parse(&self) -> Result<()> {}
 
-    //fn part1(&self) -> Result<()> {}
+    //fn part1(&self) -> Result<&str> {}
 
-    //fn part2(&self) -> Result<()> {}
+    //fn part2(&self) -> Result<&str> {}
 }
 
