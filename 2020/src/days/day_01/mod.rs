8065a7dd487e7ffddc8fce03098e823b4071d55d
Arno Velds <avelds@gmail.com>
2020-12-01 19:34:42 +0100

    improve for loop

--- 2020/src/days/day_01/mod.rs
+++ 2020/src/days/day_01/mod.rs
@@ -20,17 +20,6 @@ impl Data {
         Ok(Data(parsed.1))
     }
 
-    // n^2 complexity
-    #[allow(unused)]
-    fn prod_of_sum(&self, sum: i32) -> Result<i32> {
-        let (n1, n2) = self.0.iter()
-            .flat_map(move |n1| self.0.iter().map(move |n2| (n1, n2)))
-            .find(|(&n1, &n2)| n1 + n2 == sum)
-            .ok_or_else(|| anyhow!(format!("No 2 number add to {}", sum)))?;
-
-        Ok(n1 * n2)
-    }
-
     fn prod_of_sum2(&self, sum: i32) -> Result<i32> {
         let mut numbers = self.0.clone();
         numbers.sort();
@@ -50,10 +39,10 @@ impl Data {
         let mut numbers = self.0.clone();
         numbers.sort();
 
-        for i1 in 0..numbers.len() {
-            for i2 in i1+1..numbers.len() {
-                let wanted = sum - numbers[i1] - numbers[i2];
-                if wanted < numbers[i2] { break }
+        for (i1, &n1) in numbers.iter().enumerate() {
+            for (i2, &n2) in numbers.iter().enumerate().skip(i1) {
+                let wanted = sum - n1 - n2;
+                if wanted < n2 { break }
                 if let Ok(_) = numbers[i2+1..].binary_search(&wanted) {
                     return Ok(wanted*numbers[i1]*numbers[i2])
                 }
