e02f3319121e099a3d7c162cc85c38aa3ca77330
Arno Velds <avelds@gmail.com>
2020-12-03 12:35:55 +0100

    added run all days

--- 2020/src/lib.rs
+++ 2020/src/lib.rs
@@ -1,9 +1,10 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::fs::File;
 use std::io::Read;
 use std::time::Instant;
 
 use anyhow::{anyhow, Result};
+use tabular::{Table, row};
 
 pub trait Problem {
     #[allow(unused_variables)]
@@ -44,6 +45,36 @@ pub fn run_day<I: AsRef<Path>>(day: u32, input: I) -> Result<()> {
     let t = Instant::now();
     let res2 = p.part2()?;
     println!("Day {} part 2: {} ({:?})", day, res2, t.elapsed());
+
+    Ok(())
+}
+
+pub fn run_all() -> Result<()> {
+    let mut table = Table::new("{:<} {:<} {:<} {:<} {:<} {:<} {:<}");
+    table.add_row(row!("day", "parse", "part1", "part2", "total", "output",""));
+    let t0 = Instant::now();
+    for day in 1..25 {
+        let mut p = days::get_solution(day)?;
+        if let Ok(mut f) = File::open(PathBuf::from(format!("inputs/day_{:02}.txt", day))) {
+            let t = Instant::now();
+            let mut b = Vec::new();
+            f.read_to_end(&mut b)?;
+            p.parse(&b)?;
+            let tp = t.elapsed();
+            let t = Instant::now();
+            let res1 = p.part1()?;
+            let tp1 = t.elapsed();
+
+            let t = Instant::now();
+            let res2 = p.part2()?;
+            let tp2 = t.elapsed();
+            //println!("{:?}\t{:?}\t\t{:?}\t{:?}\t{:?}\tout:{}, {}", day,  tp, tp1, tp2, tp+tp1+tp2, res1, res2);
+            table.add_row(row!(day, format!("{:?}", tp), format!("{:?}",tp1), format!("{:?}",tp2), format!("{:?}",tp+tp1+tp2), res1, res2));
+        }
+    }
+   table.add_row(row!("all", "","","", format!("{:?}", t0.elapsed()),"",""));
+   print!("{}", table);
+   //println!("real time:\t{:?}", t0.elapsed());
 
     Ok(())
 }
--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -5,7 +5,7 @@ use chrono::prelude::*;
 use chrono::offset::FixedOffset;
 use anyhow::{anyhow, Result};
 
-use aoc2020::run_day;
+use aoc2020::{run_day, run_all};
 
 const YEAR:i32 = 2020;
 
@@ -16,28 +16,35 @@ struct Opt {
     day: Option<u32>,
     #[structopt(short = "i", long = "input", parse(from_os_str))]
     input: Option<PathBuf>,
+    #[structopt(long)]
+    all: bool,
 }
 
 fn main() -> Result<()> {
     let opt = Opt::from_args();
-    let now = Utc::now().with_timezone(&FixedOffset::west(5 * 3600));
-    let aoc_start = FixedOffset::west(5 * 3600).ymd(YEAR, 1, 1).and_hms(0,0,0);
-    let run_date = if let Some(day) = opt.day {
-        FixedOffset::west(5 * 3600).ymd(YEAR, 12, day).and_hms(0,0,0)
+
+    if opt.all {
+        run_all()
     } else {
-        now.clone()
-    };
+        let now = Utc::now().with_timezone(&FixedOffset::west(5 * 3600));
+        let aoc_start = FixedOffset::west(5 * 3600).ymd(YEAR, 1, 1).and_hms(0,0,0);
+        let run_date = if let Some(day) = opt.day {
+            FixedOffset::west(5 * 3600).ymd(YEAR, 12, day).and_hms(0,0,0)
+        } else {
+            now.clone()
+        };
 
-    if run_date < aoc_start || run_date > now {
-        return Err(anyhow!("AoC day is in the future"));
+        if run_date < aoc_start || run_date > now {
+            return Err(anyhow!("AoC day is in the future"));
+        }
+
+        let torun = run_date.day();
+        let input = opt.input.unwrap_or_else(|| PathBuf::from(format!("inputs/day_{:02}.txt", torun)));
+
+        println!("run day {} for {}", torun, input.display());
+
+        run_day(torun, input)?;
+
+        Ok(())
     }
-
-    let torun = run_date.day();
-    let input = opt.input.unwrap_or_else(|| PathBuf::from(format!("inputs/day_{:02}.txt", torun)));
-
-    println!("run day {} for {}", torun, input.display());
-
-    run_day(torun, input)?;
-
-    Ok(())
 }
