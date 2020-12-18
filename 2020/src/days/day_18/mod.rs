6bfb677af34bd0571875f93a07279324e06f1a59
Arno Velds <avelds@gmail.com>
2020-12-18 20:52:04 +0100

    cleanups and some drying

--- 2020/src/days/day_18/mod.rs
+++ 2020/src/days/day_18/mod.rs
@@ -6,13 +6,12 @@ use crate::Problem;
 
 use nom::{
     branch::alt,
-    bytes::complete::is_a,
     character::complete::{char, line_ending, one_of, space0},
     combinator::{map, iterator},
     multi::{fold_many1, separated_list1},
     combinator::all_consuming,
     sequence::{delimited, preceded, terminated, tuple},
-    IResult
+    IResult, Parser
 };
 use crate::parsers::positive_integer;
 
@@ -21,11 +20,25 @@ pub struct Solution {
     input: String
 }
 
-
 enum Op {
     Mul,
     Add,
 }
+
+//
+//both:
+fn in_brackets<'a, O, F>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, nom::error::Error<&'a str>>
+where F: Parser<&'a str, O, nom::error::Error<&'a str>>,
+{
+  delimited(char('('), inner, char(')'))
+}
+
+fn space_padded<'a, O, F>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, nom::error::Error<&'a str>>
+where F: Parser<&'a str, O, nom::error::Error<&'a str>>,
+{
+  delimited(space0, inner, space0)
+}
+
 
 //part one
 fn operator(i: &str) -> IResult<&str, Op> {
@@ -39,16 +52,12 @@ fn operator(i: &str) -> IResult<&str, Op> {
 }
 
 fn value(i: &str) -> IResult<&str, u64> {
-    alt((positive_integer, nested_expression))(i)
+    alt((positive_integer, in_brackets(no_precedence)))(i)
 }
 
-fn nested_expression(i: &str) -> IResult<&str, u64> {
-    delimited(char('('), expression, char(')'))(i)
-}
-
-fn expression(i: &str) -> IResult<&str, u64> {
+fn no_precedence(i: &str) -> IResult<&str, u64> {
     let (i, v) = value(i)?;
-    let mut it = iterator(i, tuple((terminated(preceded(space0, operator), space0), value)));
+    let mut it = iterator(i, tuple((space_padded(operator), value)));
     let result = it.fold(v, |mut acc, (op, val)| {
         match op {
             Op::Add => acc += val,
@@ -60,30 +69,24 @@ fn expression(i: &str) -> IResult<&str, u64> {
     Ok((res.0, result))
 }
 
-fn parse(i: &str) -> IResult<&str, u64> {
-    fold_many1(terminated(expression, line_ending), 0, |acc, val| {
-        acc + val
-    })(i)
-}
 
 //part 2
 fn addition(i: &str) -> IResult<&str, u64> {
-    map(separated_list1(terminated(preceded(space0, char('+')), space0), alt((positive_integer, nested_expression2))), |v| v.iter().sum())(i)
+    map(separated_list1(
+                space_padded(char('+')), 
+                alt((positive_integer, in_brackets(plus_precedence)))
+            ), |v| v.iter().sum())(i)
 }
 
 fn product(i: &str) -> IResult<&str, u64> {
-    map(preceded(terminated(preceded(space0, char('*')), space0),value2), |v| v)(i)
+    map(preceded(space_padded(char('*')),value2), |v| v)(i)
 }
 
 fn value2(i: &str) -> IResult<&str, u64> {
-    alt((addition, nested_expression2, positive_integer))(i)
+    alt((addition, in_brackets(plus_precedence), positive_integer))(i)
 }
 
-fn nested_expression2(i: &str) -> IResult<&str, u64> {
-    delimited(char('('), expression2, char(')'))(i)
-}
-
-fn expression2(i: &str) -> IResult<&str, u64> {
+fn plus_precedence(i: &str) -> IResult<&str, u64> {
     let (i, v) = value2(i)?;
     let mut it = iterator(i, product);
     let result = it.fold(v, |mut acc, val| {
@@ -95,12 +98,14 @@ fn expression2(i: &str) -> IResult<&str, u64> {
     Ok((res.0, result))
 }
 
-fn parse2(i: &str) -> IResult<&str, u64> {
-    fold_many1(terminated(expression2, line_ending), 0, |acc, val| {
+// parse according to passed parser
+fn parse<'a, F>(parser: F) -> impl FnMut(&'a str) -> IResult<&'a str, u64, nom::error::Error<&'a str>>
+where F: Parser<&'a str, u64, nom::error::Error<&'a str>>,
+{
+    all_consuming(fold_many1(terminated(parser, line_ending), 0, |acc, val| {
         acc + val
-    })(i)
+    }))
 }
-
 
 impl Problem for Solution {
     fn parse(&mut self, i: &[u8]) -> Result<()> {
@@ -109,12 +114,12 @@ impl Problem for Solution {
     }
 
     fn part1(&self) -> Result<String> {
-        let result = parse(self.input.as_str()).map_err(|e| anyhow!(e.to_string()))?;
+        let result = parse(no_precedence)(self.input.as_str()).map_err(|e| anyhow!(e.to_string()))?;
         Ok(format!("{}", result.1))
     }
 
     fn part2(&self) -> Result<String> {
-        let result = parse2(self.input.as_str()).map_err(|e| anyhow!(e.to_string()))?;
+        let result = parse(plus_precedence)(self.input.as_str()).map_err(|e| anyhow!(e.to_string()))?;
         Ok(format!("{}", result.1))
     }
 }
@@ -125,11 +130,11 @@ mod tests {
 
     #[test]
     fn p1() {
-        let s = expression("1 + 2 * 3 + 4 * 5 + 6");
+        let s = no_precedence("1 + 2 * 3 + 4 * 5 + 6");
         println!("{:?}", s);
         assert_eq!(s.unwrap().1, 71);
 
-        let s = expression("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2");
+        let s = no_precedence("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2");
         println!("{:?}", s);
         assert_eq!(s.unwrap().1, 13632);
 
@@ -137,11 +142,11 @@ mod tests {
     #[test]
     fn p2() {
 
-        let s = expression2("1 + 2 + 3");
+        let s = plus_precedence("1 + 2 + 3");
         println!("{:?}", s);
         assert_eq!(s.unwrap().1, 6);
 
-        let s = expression2("1 + 2 * 3 + 4 * 5 + 6");
+        let s = plus_precedence("1 + 2 * 3 + 4 * 5 + 6");
         println!("{:?}", s);
         assert_eq!(s.unwrap().1, 231);
 
@@ -149,7 +154,7 @@ mod tests {
         println!("{:?}", s);
         assert_eq!(s.unwrap().1, 1445);
 
-        let s = expression2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2");
+        let s = plus_precedence("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2");
         println!("{:?}", s);
         assert_eq!(s.unwrap().1, 23340);
 
